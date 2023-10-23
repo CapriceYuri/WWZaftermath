a9929b51a57d25d84e40641e4ecc418058020ad4
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-10-22 22:55:36 -0400

    Summary Page added

--- logicH.js
+++ logicH.js
@@ -1,18 +1,29 @@
 const hardSelect = document.querySelector("#btnradio1");
 const normalSelect = document.querySelector("#btnradio2")
-const normalBox = document.querySelector(".BOX1");
-const hardBox = document.querySelector(".BOX2");
+const summary = document.querySelector("#btnradio0")
+const summaryBox = document.querySelector(".BOX0")
+const normalBox = document.querySelector(".BOX2");
+const hardBox = document.querySelector(".BOX1");
 
-
+summary.addEventListener('click', () => {
+    hardSelect.checked = false;
+    hardBox.style.display = 'none'
+    normalSelect.checked = false;
+    normalBox.style.display = 'none'
+    summaryBox.style.display = 'block'
+})
 normalSelect.addEventListener('click', () => {
     hardSelect.checked = false;
-    hardBox.style.display = 'block'
-    normalBox.style.display = 'none'
-    console.log('Testing')
+    hardBox.style.display = 'none'
+    summary.checked = false;
+    summaryBox.style.display = 'none'
+    normalBox.style.display = 'block'
 })
 hardSelect.addEventListener('click', () => {
     normalSelect.checked = false;
-    hardBox.style.display = 'none'
-    normalBox.style.display = 'block'
-    console.log('Testing')
+    normalBox.style.display = 'none';
+    summary.checked = false;
+    summaryBox.style.display = 'none';
+    hardBox.style.display = 'block'
+
 })
