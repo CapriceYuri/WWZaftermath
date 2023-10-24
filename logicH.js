ced165f48f3c7694aa61472012ad06d4681c22a0
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-10-24 13:11:07 -0400

    Added section for speedrun, updated horde ranking

--- logicH.js
+++ logicH.js
@@ -5,11 +5,20 @@ const summaryBox = document.querySelector(".BOX0")
 const normalBox = document.querySelector(".BOX2");
 const hardBox = document.querySelector(".BOX1");
 
+const speedrun = document.querySelector("#speedrunE");
+const speedrunBox = document.querySelector(".BOX3")
+const speedrun2 = document.querySelector("#speedrunE2");
+const speedrunBox2 = document.querySelector(".BOX4")
+
 summary.addEventListener('click', () => {
     hardSelect.checked = false;
     hardBox.style.display = 'none'
     normalSelect.checked = false;
     normalBox.style.display = 'none'
+    speedrun.checked = false;
+    speedrunBox.style.display = 'none'
+    speedrun2.checked = false
+    speedrunBox2.style.display = 'none'
     summaryBox.style.display = 'block'
 })
 normalSelect.addEventListener('click', () => {
@@ -17,6 +26,10 @@ normalSelect.addEventListener('click', () => {
     hardBox.style.display = 'none'
     summary.checked = false;
     summaryBox.style.display = 'none'
+    speedrun.checked = false;
+    speedrunBox.style.display = 'none'
+    speedrun2.checked = false
+    speedrunBox2.style.display = 'none'
     normalBox.style.display = 'block'
 })
 hardSelect.addEventListener('click', () => {
@@ -24,6 +37,32 @@ hardSelect.addEventListener('click', () => {
     normalBox.style.display = 'none';
     summary.checked = false;
     summaryBox.style.display = 'none';
+    speedrun.checked = false;
+    speedrunBox.style.display = 'none'
+    speedrun2.checked = false
+    speedrunBox2.style.display = 'none'
     hardBox.style.display = 'block'
 
+})
+speedrun.addEventListener('click', () => {
+    hardSelect.checked = false;
+    hardBox.style.display = 'none'
+    normalSelect.checked = false;
+    normalBox.style.display = 'none'
+    summary.checked = false
+    summaryBox.style.display = 'none'
+    speedrun2.checked = false
+    speedrunBox2.style.display = 'none'
+    speedrunBox.style.display = 'block'
+})
+speedrun2.addEventListener('click', () => {
+    hardSelect.checked = false;
+    hardBox.style.display = 'none'
+    normalSelect.checked = false;
+    normalBox.style.display = 'none'
+    summary.checked = false
+    summaryBox.style.display = 'none'
+    speedrun.checked = false
+    speedrunBox.style.display = 'none'
+    speedrunBox2.style.display = 'block'
 })
